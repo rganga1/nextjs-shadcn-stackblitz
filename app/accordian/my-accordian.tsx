792b1443cdd324dcfffc49740804c21c7179da3d
Rakesh G <rockingrakeshshetty@gmail.com>
2024-08-25 21:16:35 +0530

    temp commit for accordian

---
+++ app/accordian/my-accordian.tsx
@@ -0,0 +1,9 @@
+const QA = {
+  q1: "a1",
+  q2: "a2",
+  q3: "a3",
+};
+
+export default function MyAccordion() {
+  return <div></div>;
+}
---
+++ app/accordian/radix-accordian.tsx
@@ -0,0 +1,19 @@
+// index.jsx
+import * as Accordion from "@radix-ui/react-accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+
+export default function Accordian() {
+  return (
+    <Accordion.Root type="single">
+      <Accordion.Item value="item-1">
+        <Accordion.Header>
+          <Accordion.Trigger className="AccordionTrigger">
+            <span>Trigger text</span>
+            <ChevronDownIcon className="" aria-hidden />
+          </Accordion.Trigger>
+        </Accordion.Header>
+        <Accordion.Content>Collapsible content</Accordion.Content>
+      </Accordion.Item>
+    </Accordion.Root>
+  );
+}
